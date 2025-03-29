1fd88efff6117a9171b19efc8219405688a60703
MahmoudDiaa2000 <mahmouddiaa874@gmail.com>
2025-03-29 08:24:55 +0200

    add custom app bar to home view

--- lib/Features/splash/presentation/view_models/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/view_models/views/widgets/splash_view_body.dart
@@ -87,9 +87,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
   }
 
   void navigateToHome() {
-    Future.delayed(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 5), () {
       Get.to(() => const HomeView(),
-          transition: Transition.fade, duration: Duration(seconds: 5));
+          transition: Transition.fade, duration: Duration(seconds: 3));
     });
   }
 }
