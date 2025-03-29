5868cfb256a3f06b1ad6b5336049ad647d5ecda7
MahmoudDiaa2000 <mahmouddiaa874@gmail.com>
2025-03-29 12:33:55 +0200

    add custom list viwe item to home view

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,10 @@
 class AssetsData {
   static const logo = 'assets/images/open-book-icon.png';
+
+  static const book1 = 'assets/images/test_book1.jpg';
+  static const book2 = 'assets/images/test_book2.jpg';
+  static const book3 = 'assets/images/test_book3.jpg';
+  static const book4 = 'assets/images/test_book4.jpg';
+  static const book5 = 'assets/images/test_book5.jpg';
+  static const book6 = 'assets/images/test_book6.jpg';
 }
