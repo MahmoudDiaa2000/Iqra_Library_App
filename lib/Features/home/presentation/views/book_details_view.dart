9b2be4079a92ff85b2cd7be99016ba7809df1361
MahmoudDiaa2000 <mahmouddiaa874@gmail.com>
2025-04-25 04:41:36 +0300

    create book_details_view.dart

---
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:iqra_library_app/Features/home/presentation/views/widgets/book_details_view_body.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: BookDetailsViewBody());
+  }
+}
