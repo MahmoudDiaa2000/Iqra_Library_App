73bde2596c38f9da5ce0862094f0bf8121cb9215
MahmoudDiaa2000 <mahmouddiaa874@gmail.com>
2025-04-26 09:22:25 +0300

    add title and subtitle to details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iqra_library_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:iqra_library_app/Features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:iqra_library_app/core/utils/styles.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -19,7 +20,23 @@ class BookDetailsViewBody extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * 0.17),
             child: CustomBookImage(),
-          )
+
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text('Clean Code',
+            style: Styles.textStyle30,),
+          const SizedBox(
+            height: 6,
+          ),
+          Text(' Robert C. Martin',
+            style: Styles.textStyle18.copyWith(
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic
+            ),),
+
+
 
         ],
       ),
