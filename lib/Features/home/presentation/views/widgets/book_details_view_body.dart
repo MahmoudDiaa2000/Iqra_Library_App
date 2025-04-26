5596fab2ad07f0f87def6e33f10fb51ca4293aae
MahmoudDiaa2000 <mahmouddiaa874@gmail.com>
2025-04-26 19:01:32 +0300

    create books action

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:iqra_library_app/Features/home/presentation/views/widgets/book_r
 import 'package:iqra_library_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:iqra_library_app/Features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:iqra_library_app/core/utils/styles.dart';
+import 'package:iqra_library_app/core/widgets/custom_button.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key });
@@ -38,7 +39,11 @@ class BookDetailsViewBody extends StatelessWidget {
           const SizedBox(
             height: 18,
           ),
-          BookRating(mainAxisAlignment: MainAxisAlignment.center,)
+          BookRating(mainAxisAlignment: MainAxisAlignment.center,),
+          const SizedBox(
+            height: 40,
+          ),
+          const BooksAction()
 
 
 
@@ -49,4 +54,39 @@ class BookDetailsViewBody extends StatelessWidget {
 }
 
 
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: [
+          Expanded(child:
+          CustomButton(
+            text: '20.00\$',
+            backgroundColor: Colors.grey,
+            textColor: Colors.black87,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                bottomLeft: Radius.circular(16)
+            ),
+          ),
+          ),
+          Expanded(child:
+          CustomButton(
+            text: 'Free preview',
+            backgroundColor: Colors.amber,
+            textColor: Colors.white,
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(16),
+                bottomRight: Radius.circular(16)
+            ),
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
